27673a5b499634c364123b56daf7578d88b6eee9
MariaKar1991 <mariaskaravazaki@gmail.com>
2024-10-09 11:36:00 +0100

    add max width and min in about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -14,26 +14,27 @@ export default function About(): JSX.Element {
           </h2>
         </div>
       </div>
-      <div className="about-paragraph-container max-w-[80%]">
-        <p className="about-paragraph text-white text-justify leading-7 w-[70%] mx-auto tracking-tight word-spacing-[0.1rem]">
-          I'm a bright, motivated, and enthusiastic web{" "}
-          <ReactTyped
-            className="inline-block"
-            strings={["developer.", "hero."]}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          />
-        </p>
-        <p className="about-paragraph text-white text-justify leading-7 w-[70%] mx-auto tracking-tight word-spacing-[0.1rem]">
-          I'm passionate about React, Angular, Node.js, Next.js, Jest, UX/UI,
-          and MongoDB, learning new technologies and the software development
-          techniques needed to write the best quality code.
-          <br />
-          Overall, my goal is to provide end-to-end solutions that not only meet
-          but exceed the expectations of my clients, delivering innovative and
-          reliable web applications.
-        </p>
+      <div className="max-w-[600px] min-w-[300px]">
+        <div className="about-paragraph-container flex flex-col items-center justify-center">
+          <p className="about-paragraph text-white text-justify leading-7 w-[70%] mx-auto tracking-tight word-spacing-[0.1rem]">
+            I'm a bright, motivated, and enthusiastic web{" "}
+            <ReactTyped
+              className="inline-block"
+              strings={["developer.", "hero."]}
+              typeSpeed={120}
+              backSpeed={140}
+              loop
+            />
+          </p>
+          <p className="about-paragraph text-white text-justify leading-7 w-[70%] mx-auto tracking-tight word-spacing-[0.1rem]">
+            I'm passionate about React, Angular, Node.js, Next.js, Jest, UX/UI,
+            and MongoDB, learning new technologies and the software development
+            techniques needed to write the best quality code. Overall, my goal
+            is to provide end-to-end solutions that not only meet but exceed the
+            expectations of my clients, delivering innovative and reliable web
+            applications.
+          </p>
+        </div>
       </div>
     </div>
   );
